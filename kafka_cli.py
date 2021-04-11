abaaf1ea0ae067158bc7e5281dd56f5c45420bfa
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-12 00:06:05 +0430

    timeout not via env

--- kafka_cli.py
+++ kafka_cli.py
@@ -24,7 +24,7 @@ consumer = KafkaConsumer(
     group_id=KAFKA_TOPIC_CONSUMER_GROUP,
     auto_offset_reset='latest',
     enable_auto_commit=True,
-    session_timeout_ms=KAFKA_CONSUMER_HEART_BEAT_TIMEOUT
+    session_timeout_ms=900000#KAFKA_CONSUMER_HEART_BEAT_TIMEOUT
 )
 
 producer = KafkaProducer(
