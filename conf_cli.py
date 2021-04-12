268e0ca5e73115d9db49ca7b68fec3949e78a93e
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-12 22:15:58 +0430

    confluent kafak client

---
+++ conf_cli.py
@@ -0,0 +1,44 @@
+from confluent_kafka import Consumer
+import logging
+
+logger=logging.getLogger("kafka")
+
+KAFKA_ENDPOINT = getenv('KAFKA_ENDPOINT')
+KAFKA_TOPIC_CONSUMER_GROUP = getenv('KAFKA_TOPIC_CONSUMER_GROUP')
+KAFKA_CONSUMER_HEART_BEAT_TIMEOUT = int(getenv('KAFKA_CONSUMER_HEART_BEAT_TIMEOUT'))
+maximum_count_of_try_to_commit = 6
+
+
+class Topics(enum.Enum):
+    EVENTS = getenv('KAFKA_TOPIC_EVENTS')
+    PLAY_GAME = getenv('KAFKA_TOPIC_PLAY_GAME')
+
+
+
+c = Consumer({
+    'bootstrap.servers': KAFKA_ENDPOINT,
+    'group.id': KAFKA_TOPIC_CONSUMER_GROUP,
+    'auto.offset.reset': 'latest',
+    'enable.auto.commit': True,
+    'session.timeout.ms': 10*1000,      #10 seconds
+    'max.poll.interval.ms': 30*60*1000  #30 minutes
+    'heartbeat.interval.ms': 1*1000     #1 seconds
+})
+
+c.subscribe([Topics.PLAY_GAME.value])
+
+
+def get_message():
+    
+    msg = c.poll()
+
+    if msg is None:
+        continue
+    if msg.error():
+        logger.error(f"error acurred while fetching new message: {msg.error()}")
+        print()
+        continue
+    return msg.value().decode('utf-8')
+
+def close():
+    c.close()
