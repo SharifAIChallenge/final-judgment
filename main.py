b3f88f3e888ae9249da442e57b5546ce1a8b1be7
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-12 16:02:25 +0430

    info to warning

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ for message in kcli.get_consumer():
         # tokens.append(command['game_id'])
         
         log.new_token_logger(command['game_id'])
-        logger.info(f"got new record:{command}")
+        logger.warning(f"got new record:{command}")
         
         kcli.push_event(Event(token=command['game_id'], status_code=EventStatus.MATCH_STARTED.value,
                               title='match started successfully!').__dict__)
         events = judge(players=command['player_ids'], game_id=command['game_id'], map_id=command['map_id'])
         
-        logger.info(f"resulting events are:{len(events)}")
-        [logger.info(event.title) for event in events]
+        logger.warning(f"resulting events are:{len(events)}")
+        [logger.warning(event.title) for event in events]
 
         [kcli.push_event(event.__dict__) for event in events]
 
