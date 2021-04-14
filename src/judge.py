c326eaf5771e4f64955a7368b4581d1f7dd6cae7
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-15 03:35:33 +0430

    update match_runcommand

--- src/judge.py
+++ src/judge.py
@@ -13,7 +13,7 @@ logger=logging.getLogger("judge")
 STATS_KEYNAME = "stats"
 
 match_timeout= int(os.getenv("MATCH_TIMEOUT"))
-match_runcommand=["match", "--first-team='spawn1'", "--second-team='spawn2'", "--read-map=map"]
+match_runcommand=["match", "--first-team=spawn1", "--second-team=spawn2", "--read-map=map"]
 match_base_dir="/usr/local/match"
 match_record_path = f"{match_base_dir}/log.json"
 match_log_path = f"{match_base_dir}/Log/server/server.log"
