89f80bc9bcc7075e99ccf97b3b578022d6f166d7
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-15 00:45:59 +0430

    some few touches

--- main.py
+++ main.py
@@ -16,17 +16,18 @@ while True:
         if not message:
             continue
 
-        logger.info(f"message is: {message}")
         command = json.loads(message.value().decode('utf-8'))
         logger.info(f"command is:{command}")
-        token=command['game_id']
         
-        log.new_token_logger(command['game_id'])
+        token=command['game_id']
+        players=command['game_ids']
+        map_id=command['map_id']
+        
+        log.new_token_logger(token)
         logger.info(f"got new record:{command}")
         
-        events.push(events.Event(token=command['game_id'], status_code=events.EventStatus.MATCH_STARTED.value,
-                              title='match started successfully!'))
-        event_list = judge(players=command['player_ids'], game_id=command['game_id'], map_id=command['map_id'])
+        events.push(events.Event(token=token, status_code=events.EventStatus.MATCH_STARTED.value,title='match started successfully!'))
+        event_list = judge(players=players, game_id=token, map_id=map_id)
         logger.info(f"resulting events are:{len(event_list)}")
       
         events.push_all(event_list)
