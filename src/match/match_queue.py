b2af3cf087d17d1a810edd6979dfbf43c8813750
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-25 00:25:24 +0430

    dont know

--- src/match/match_queue.py
+++ src/match/match_queue.py
@@ -18,8 +18,7 @@ match_consumer = Consumer({
     'group.id': KAFKA_TOPIC_CONSUMER_GROUP,
     'auto.offset.reset': 'latest',
     'enable.auto.offset.store':False,
-    'client.id': gethostname(),
-    'consumer.id': gethostname(),
+    'client.id': f'{gethostname()}',
     'enable.auto.commit': False,
     'session.timeout.ms': 10*1000,      #10 seconds
     'max.poll.interval.ms': 30*60*1000,  #30 minutes
