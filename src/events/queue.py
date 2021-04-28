c238f9ec8630cc83f14440c63a3f5db6fdd4d593
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-28 11:25:23 +0430

    revert to default event queue

--- src/events/queue.py
+++ src/events/queue.py
@@ -9,20 +9,17 @@ KAFKA_ENDPOINT = getenv('KAFKA_ENDPOINT')
 KAFKA_TOPIC_EVENTS = getenv('KAFKA_TOPIC_EVENTS')
 
 
-p = Producer({
-    'bootstrap.servers': KAFKA_ENDPOINT,
-    'value.serializer': lambda dic:json.dumps(dic).encode('utf-8') 
-    })
+p = Producer({'bootstrap.servers': KAFKA_ENDPOINT})
 
-# def __serilize(dic):
-#     return json.dumps(dic).encode('utf-8')
+def __serilize(dic):
+    return json.dumps(dic).encode('utf-8')
 
 def __on_deliver(err,msg):
     """ Called once for each message produced to indicate delivery result.
     Triggered by poll() or flush(). """
     if err is not None:
         logger.warn(f"failed to push event : [{msg}]")
-        logger.warn(f"failed with error : [{err}]")        
+        logger.warn(f"failed with error : [{err}]")
     else:
         logger.info(f"event pushed successfully")
 
@@ -35,7 +32,7 @@ def __push_data(data):
     # Asynchronously produce a message, the delivery report callback
     # will be triggered from poll() above, or flush() below, when the message has
     # been successfully delivered or failed permanently.
-    p.produce(KAFKA_TOPIC_EVENTS, data, callback=__on_deliver)
+    p.produce(KAFKA_TOPIC_EVENTS, __serilize(data), callback=__on_deliver)
 
 def push(event):
     __push_data(event.__dict__)
@@ -44,5 +41,3 @@ def push(event):
 def push_all(events):
     [__push_data(event.__dict__) for event in events]
     p.flush()
-
-
